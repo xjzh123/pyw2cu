41e71ba9e2ee8f845eeb66d5f590b5d40e753a9c
xjzh123 <37945610+xjzh123@users.noreply.github.com>
2023-01-14 05:47:23 +0000

    works

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_file, redirect, jsonify
+from flask import Flask, request, send_file
 from sys import gettrace
 from gevent import pywsgi
 import logging
@@ -10,23 +10,23 @@ app = Flask(__name__)
 
 count = 0
 
+
 @app.route('/upload', methods=['POST'])
 def upload():
-    app.logger.info('upload')
-    file = request.files.get('file')
-    with open(f'/photos/{time.strftime("%Y-%m-%d %H %M %S")}+{count}.png', 'w') as f:
-        f.write(file)
+  app.logger.info('upload')
+  file = request.files.get('file')
+  file.save(f'photos/{time.strftime("%Y-%m-%d %H %M %S")} [{count}].png')
 
 
 @app.route('/')
 def index():
-    return send_file('index.html')
+  return send_file('index.html')
 
 
 if __name__ == '__main__':
-    app.logger.info('started')
-    if gettrace():  # check whether program is running under debug mode
-        app.run(host='0.0.0.0')
-    else:
-        server = pywsgi.WSGIServer(('0.0.0.0', 5000), app)
-        server.serve_forever()
+  app.logger.info('started')
+  if gettrace():  # check whether program is running under debug mode
+    app.run(host='0.0.0.0')
+  else:
+    server = pywsgi.WSGIServer(('0.0.0.0', 5000), app)
+    server.serve_forever()
